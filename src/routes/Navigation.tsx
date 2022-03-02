154eff51c1512ae2ed0d8be7a5aa1a4055d14e00
Enrique Briones <enr.briones@outlook.cl>
2022-03-01 21:00:17 -0300

    fix imports

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,7 +4,6 @@ import {
   Route,
   NavLink
 } from 'react-router-dom';
-import ShoppingPage from '../02-components-patterns/pages/ShoppingPage';
 
 import logo from '../logo.svg';
 import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
