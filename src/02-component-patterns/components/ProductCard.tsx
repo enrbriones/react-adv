834676f8e586b1faf3e2ee6c4802ffd0d356624a
Enrique Briones <enr.briones@outlook.cl>
2022-03-02 21:37:07 -0300

    Component Extend Styles - listo

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,27 +1,34 @@
-import { createContext } from 'react';
+import { createContext, ReactElement } from "react";
 
-import { useProduct } from '../hooks/useProduct';
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
+import { useProduct } from "../hooks/useProduct";
+import { ProductContextProps, Product } from "../interfaces/interfaces";
 
-import styles from '../styles/styles.module.css'
+import styles from "../styles/styles.module.css";
+
+export interface Props {
+  product: Product;
+  children?: ReactElement | ReactElement[];
+  className?: string;
+  style?: React.CSSProperties;
+}
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
+export const ProductCard = ({ children, product, className, style }: Props) => {
+  const { counter, increaseBy } = useProduct();
 
-export const ProductCard = ({ children, product }: ProductCardProps ) => {
-
-    const { counter, increaseBy } = useProduct();
-
-    return (
-        <Provider value={{
-            counter,
-            increaseBy,
-            product
-        }}>
-            <div className={ styles.productCard }>
-                { children }
-            </div>
-        </Provider>
-    )
-}
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={`${styles.productCard} ${className}`} style={style}>
+        {children}
+      </div>
+    </Provider>
+  );
+};
